42b1aa789459069178beba20a087a17d376189de
mmaatteusz <61252899+mmaatteusz@users.noreply.github.com>
2026-09-25 22:12:26 +0200

    Format map interaction regression test

--- mobile/test/map_interaction_test.dart
+++ mobile/test/map_interaction_test.dart
@@ -3,14 +3,17 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('embedded maps eagerly claim gestures from surrounding scroll views', () {
-    final recognizers = mapGestureRecognizers();
+  test(
+    'embedded maps eagerly claim gestures from surrounding scroll views',
+    () {
+      final recognizers = mapGestureRecognizers();
 
-    expect(recognizers, hasLength(1));
-    expect(recognizers.single.type, EagerGestureRecognizer);
+      expect(recognizers, hasLength(1));
+      expect(recognizers.single.type, EagerGestureRecognizer);
 
-    final recognizer = recognizers.single.constructor();
-    expect(recognizer, isA<EagerGestureRecognizer>());
-    recognizer.dispose();
-  });
+      final recognizer = recognizers.single.constructor();
+      expect(recognizer, isA<EagerGestureRecognizer>());
+      recognizer.dispose();
+    },
+  );
 }
